6dc7c180221556548958787cad60b709e03016de
serilen <serilen29@gmail.com>
2021-10-19 23:48:45 +0300

    add escape and style textarea

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 document.body.addEventListener("keydown", keyIncl);
-
+let initialTextContentDiv = document.getElementById('box').innerHTML; //в переменную запоминаем первоначальное значение дива
 function keyIncl(event) {
+
             //если нажаты S или E + ctrl
             if (((event.code === "KeyS" || event.code === "KeyE") && event.ctrlKey) || event.code === "Escape") {
                 event.preventDefault(); //отменяем встроенные действия горячих клавиш для браузера
-
+                
                 let el = document.querySelector("hr").nextElementSibling; //объявляем элемент после hr
 
                 if (event.code === "KeyE" && el.nodeName === "DIV") {
                     let newEl = document.createElement("textarea");
+                    newEl.style.cssText=`
+                    border: none;
+                    overflow: hidden;
+                    outline: none;
+                    resize: none;
+                    height: 100vw;                    
+                    `;
+
                     newEl.textContent = el.textContent;//Если нажата Е и тип узла после узла hr див (обязательно большими буквами), то создаем новый элемент Текстареа с новым содержимым 
                     el.replaceWith(newEl);  // меняем содержимое
                 }
@@ -22,8 +31,7 @@ function keyIncl(event) {
                 else if (event.code === "Escape" && el.nodeName === "TEXTAREA") {
 
                  let newEl = document.createElement("div");
-                    newEl.textContent = document.getElementById("box").value;  //Если нажата Escape и тип узла после узла hr текстареа (обязательно большими буквами - название узла), то создаем новый див с новым содержимым взятым из первоначального дива
-                    // Не работает! Где ошибка?
+                    newEl.textContent = initialTextContentDiv;  //Если нажата Escape и тип узла после узла hr текстареа (обязательно большими буквами - название узла), то создаем новый див с новым содержимым взятым из первоначального дива
                     el.replaceWith(newEl); // меняем содержимое
                 }
             }
